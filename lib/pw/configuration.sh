6428372719df28abf25794cb67d96a176308ead4
Gioele Barabucci <gioele@svario.it>
2014-06-09 16:55:43 +0200

    Search for a config file in all XDG_CONFIG_DIRS
    Fixes #9.

--- lib/pw/configuration.sh
+++ lib/pw/configuration.sh
@@ -1,4 +1,12 @@
 DB=${XDG_DATA_HOME:-$HOME/.local/share}/pw/passwords
 CONFIG_FILE=${XDG_CONFIG_HOME:-$HOME/.config}/pw
 
-[ -f $CONFIG_FILE ] && . $CONFIG_FILE || true
+CONFIG_DIRS=$(echo "${XDG_CONFIG_DIRS:-/etc/xdg}" | tr ':' '\n')
+CONFIG_DIRS=$(printf '%s\n%s' "$CONFIG_DIRS" "${XDG_CONFIG_HOME:-$HOME/.config}")
+
+old_ifs=$IFS
+IFS=$(printf '\n\b')
+for dir in $CONFIG_DIRS ; do
+	[ -f "$dir/pw" ] && . "$dir/pw" || true
+done
+IFS=$old_ifs
