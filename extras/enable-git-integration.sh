b82d769c161931c97a0627c26bd9fd52e696fdee
Gioele Barabucci <gioele@svario.it>
2014-07-12 18:13:45 +0200

    Use pw-diff for fast diff in git

--- extras/enable-git-integration.sh
+++ extras/enable-git-integration.sh
@@ -5,9 +5,13 @@ git config --global merge.pw.name 'Append-only merge strategy for pw databases'
 git config --global merge.pw.driver 'tmp=$(mktemp) ; (comm -12 %A %B ; comm -13 %O %A ; comm -13 %O %B ) >| $tmp ; mv $tmp %A'
 
 # set up the diff program for password files
-
-# FIXME: use pw-diff instead of textconv
-git config --global diff.pw.textconv "pw-show -c -d"
+if [ "$1" = '--textconv' ] ; then
+	git config --global --unset diff.pw.command
+	git config --global diff.pw.textconv 'pw-show -c -d'
+else
+	git config --global diff.pw.command 'pw-diff -d $LOCAL $REMOTE'
+	git config --global --unset diff.pw.textconv
+fi
 
 db_dir=${XDG_DATA_HOME:-$HOME/.local/share}/pw
 grep -q '^* diff=pw merge=pw$' $db_dir/.gitattributes || \
